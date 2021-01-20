0dbc0cc46fb3469eeab7f384d704de368fccf2c8
closetool <c299999999@qq.com>
2021-01-20 14:51:00 +0800

    feat: add github ci action

--- src/models.rs
+++ src/models.rs
@@ -1,11 +1,11 @@
 use serde::{Deserialize, Serialize};
-use std::collections::BTreeMap;
+use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize, Debug)]
-pub(crate) struct Policies(pub(crate) BTreeMap<String, Vec<Vec<String>>>);
+pub(crate) struct Policies(pub(crate) HashMap<String, Vec<Vec<String>>>);
 
 impl Policies {
     pub fn new() -> Policies {
-        Policies(BTreeMap::new())
+        Policies(HashMap::new())
     }
 }
